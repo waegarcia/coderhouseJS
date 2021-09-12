cc2cdf3a71cc35a130118ce3dccfa9bcbf2499ba
Walter Garcia <waegarcia@gmail.com>
2021-09-12 00:30:05 -0300

    correccion rutas endpoint.js y carrito.html

--- js/api/endpoint.js
+++ js/api/endpoint.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $.get("../js/bd/componentesPC.json", function(respuesta, status){            
+    $.get("./js/bd/componentesPC.json", function(respuesta, status){            
         if(status === "success"){
             sessionStorage.setItem("API", JSON.stringify(respuesta));
         }
